262c2b72f5bc0fd67eee8cad0a163c0dfeacfa49
Dmitrij Drandarov <dmitrij.drandarov@outlook.com>
2022-08-13 23:15:23 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+    id("io.pzstorm.capsid") version "0.4.2"
+}
+
+group = "io.drandarov"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "zomboid-sdk"
+
